d8296376d8a68582711e3b95f0aa8ca2c060de49
aus10blair <56124294+aus10blair@users.noreply.github.com>
2021-07-16 15:54:43 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import './navBar.css';
+import './navBar.js';
+function App() {
+  return (
+    <div className="name">
+      <header className="-navBar">
+        <br></br>
+       
+        
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ navBar.js
@@ -0,0 +1,13 @@
+/*import React from 'react'
+
+export default function navBar() {
+function myFunction() {
+    var x = document.getElementById("topnavBar");
+    if (x.className === "navBar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navBar";
+    }
+  }
+}
+*/
